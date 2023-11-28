c3a1d8d86c7d3a8fc0acef60bfcf14d4ff8c54af
Alwin <2220707@slu.edu.ph>
2023-11-28 22:54:02 +0800

    NODE CSS NAW WORK

--- BBCLive/Manager/app.js
+++ BBCLive/Manager/app.js
@@ -8,9 +8,8 @@ const app = express();
 const port = 3000;
 
 // Serve static files from the specified directories
-app.use(express.static(path.join(__dirname, './../../../BBCLive')));
+app.use(express.static(path.join(__dirname, './../../BBCLive/')))
 app.use('/vendor/fontawesome-free', express.static(path.join(__dirname, 'node_modules/fontawesome-free')));
-app.use('/adminstyles', express.static(path.join(__dirname, './../../BBCLive/Manager/Admin/adminstyles')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
