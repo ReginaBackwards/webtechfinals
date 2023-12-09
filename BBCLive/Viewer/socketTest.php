c816d51ee095f01cc5a482797ae280c48ced23ea
jabs <2220836@slu.edu.ph>
2023-12-10 02:31:19 +0800

    NEGRO NANAY MO

--- BBCLive/Viewer/socketTest.php
+++ BBCLive/Viewer/socketTest.php
@@ -5,6 +5,7 @@ const socket = new WebSocket('ws://localhost:5000');
 const videoElement = document.getElementById('my-video');
 
 socket.onmessage = (event) => {
+    console.log('Received raw data:', event.data);  // Log raw data
   const data = JSON.parse(event.data);
   const videoUrl = data.videoUrl;
   const currentTime = data.currentTime;
@@ -14,20 +15,10 @@ socket.onmessage = (event) => {
   // Set video URL and current time
   videoElement.src = videoUrl;
   videoElement.currentTime = currentTime;
-
-  // Check if the video is currently playing
-  if (!videoElement.paused) {
-    // Video is playing, do nothing for now
-    console.log('Video is playing, no need to fetch resources.');
-  } else {
-    // Video is not playing, fetch resources
-    fetchResources();
-  }
 };
 
 socket.onopen = () => {
   console.log('WebSocket connection opened');
-  fetchInitialVideoInfo();
 };
 
 socket.onclose = (event) => {
@@ -37,32 +28,4 @@ socket.onclose = (event) => {
 // Disable controls and enable autoplay
 videoElement.controls = false;
 videoElement.autoplay = true;
-
-// Function to fetch initial video information
-function fetchInitialVideoInfo() {
-  fetch('/fetchInitialVideoInfo')  // Replace with the actual endpoint to fetch video info
-    .then((response) => response.json())
-    .then((data) => {
-      const videoUrl = data.videoUrl;
-      const currentTime = data.currentTime;
-
-      console.log('Fetched initial video info:', { videoUrl, currentTime });
-
-      // Set video URL and current time
-      videoElement.src = videoUrl;
-      videoElement.currentTime = currentTime;
-
-      // Send the initial video info to the server through WebSocket
-      socket.send(JSON.stringify({ videoUrl, currentTime }));
-    })
-    .catch((error) => {
-      console.error('Error fetching initial video info:', error);
-    });
-}
-
-// Function to fetch resources
-function fetchResources() {
-  console.log('Fetching resources...');
-  // Add your logic to fetch resources here
-}
 </script>
