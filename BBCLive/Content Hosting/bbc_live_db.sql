f6095a0dbabd29f02e4ed144dedd41b0d4339bbe
jabs <2220836@slu.edu.ph>
2023-12-15 10:02:29 +0800

    db

--- BBCLive/Content Hosting/bbc_live_db.sql
+++ BBCLive/Content Hosting/bbc_live_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 09, 2023 at 09:50 PM
+-- Generation Time: Dec 15, 2023 at 12:09 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -38,17 +38,6 @@ CREATE TABLE IF NOT EXISTS `resources` (
   KEY `author` (`author`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `resources`
---
-
-INSERT INTO `resources` (`filename`, `filepath`, `author`, `dateuploaded`, `type`) VALUES
-('10 Second Timer (1).mp4', '/Content Hosting/videos/clips/alwin/10 Second Timer (1).mp4', 'alwin', '2023-12-09', 'videos'),
-('Dame Pol (1).mp4', '/Content Hosting/videos/clips/alwin/Dame Pol (1).mp4', 'alwin', '2023-12-09', 'videos'),
-('foodp2 (1).mp4', '/Content Hosting/videos/clips/alwin/foodp2 (1).mp4', 'alwin', '2023-12-09', 'videos'),
-('Tim Henson _ Polyphia All Falls Apart Playthrough _ Ibanez TOD10 (720p) (1).mp4', '/Content Hosting/videos/clips/alwin/Tim Henson _ Polyphia All Falls Apart Playthrough _ Ibanez TOD10', 'alwin', '2023-12-09', 'videos'),
-('VID20211128201054 (1).mp4', '/Content Hosting/videos/clips/alwin/VID20211128201054 (1).mp4', 'alwin', '2023-12-09', 'videos');
-
 -- --------------------------------------------------------
 
 --
@@ -62,20 +51,8 @@ CREATE TABLE IF NOT EXISTS `scenes` (
   `date` date NOT NULL,
   `starttime` time NOT NULL,
   `endtime` time NOT NULL,
-  PRIMARY KEY (`scenename`)
+  PRIMARY KEY (`scenename`,`date`,`starttime`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `scenes`
---
-
-INSERT INTO `scenes` (`scenename`, `filepath`, `date`, `starttime`, `endtime`) VALUES
-('Lupang Hinirang', '/Content Hosting/videos/scenes/lupangHinirang.mp4', '2023-12-08', '18:56:00', '01:20:44'),
-('Bing Chilling', '/Content Hosting/videos/scenes/bingChilling.mp4', '2023-12-10', '05:05:20', '05:05:30'),
-('Hood Naruto', '/Content Hosting/videos/scenes/hoodNaruto.mp4', '2023-12-10', '05:05:31', '05:05:00'),
-('Dame Pol (1).mp4', 'Content%20Hosting\\videos\\clips\\alwin\\Dame%20Pol%20(1).mp4', '2023-12-09', '08:00:40', '08:00:56'),
-('foodp2 (1).mp4', 'Content%20Hosting\\videos\\clips\\alwin\\foodp2%20(1).mp4', '2023-12-09', '08:00:30', '08:00:40'),
-('VID20211128201054 (1).mp4', 'Content%20Hosting\\videos\\clips\\alwin\\VID20211128201054%20(1).mp4', '2023-12-09', '08:00:00', '08:00:30');
 
 -- --------------------------------------------------------
 
@@ -96,13 +73,13 @@ CREATE TABLE IF NOT EXISTS `schedules` (
 --
 
 INSERT INTO `schedules` (`day`, `username`) VALUES
+('Friday', 'alwin'),
 ('Monday', 'alwin'),
-('Wednesday', 'alwin'),
-('Friday', 'DABABY'),
-('Saturday', 'DABABY'),
-('Sunday', 'DABABY'),
-('Thursday', 'DABABY'),
-('Tuesday', 'DABABY');
+('Saturday', 'alwin'),
+('Sunday', 'alwin'),
+('Thursday', 'alwin'),
+('Tuesday', 'alwin'),
+('Wednesday', 'alwin');
 
 -- --------------------------------------------------------
 
@@ -130,12 +107,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`username`, `dp`, `password`, `firstname`, `lastname`, `role`, `banstatus`, `sessions`) VALUES
 ('alwin', '/res/avatars/alwin.jpg', 'alwin', 'Marvin', 'Rosanto', 'cm', 0, 0),
 ('ami', '/res/avatars/ami.jpg', 'ami', 'America', 'Slay', 'cm', 0, 0),
-('asfasf', '/res/avatars/default.png', 'asfasf', '   awyhawtgawsg', '   ashgasgasg              !!!!!!', 'cm', 0, 0),
-('DABABY', '/res/avatars/default.png', 'DABABY', 'BABY', 'DC', 'cm', 0, 0),
-('fettuccine', '/res/avatars/default.png', 'fettuccine', 'Abby', 'Lacorte', 'cm', 1, 0),
-('heehee123', '/res/avatars/default.png', 'heehee123', 'Jichael', 'Mackson', 'cm', 0, 0),
-('jabsTV', '/res/avatars/jabsTV.jpg', 'jabsTV123', 'Jabs', 'TV', 'admin', 0, 0),
-('MYIDOL123', '/res/avatars/default.png', 'MYIDOL123', 'LEO', 'PALA', 'cm', 0, 0);
+('jabsTV', '/res/avatars/jabsTV.jpg', 'jabsTV123', 'Jabs', 'TV', 'admin', 0, 0);
 
 --
 -- Constraints for dumped tables
